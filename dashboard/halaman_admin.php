ed1a385377869d42e2466d299b805366476f5561
Hafiyyan Abdul Aziz <hafiyyan.123@gmail.com>
2020-04-03 17:05:26 +0700

    Perbaikan login dn logout

--- dashboard/halaman_admin.php
+++ dashboard/halaman_admin.php
@@ -1,27 +1,30 @@
 <?php
 
-    echo"halaman admin";
+echo "halaman admin";
 
-	session_start();
-	if($_SESSION['status']!="login"){
-		header("location:login.php");
-	}
+session_start();
+if ($_SESSION['status'] != "login") {
+    header("location:../login");
+}
 
 ?>
 
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>WEB PERHITUNGAN BMI</title>
-    </head>
-    <body>
-        <h2>Welcome</h2>
-        <h4>Welcome, <?php echo $_SESSION['username']; ?>! You're successfull to login !</h4>
-        <br/>
-        <br/>
-        <table>
-			<td><a href="logout.php">LOGOUT</a></td>
-			<!--<td><a href="menuperhitungan"-->
-		</table>
-    </body>
+
+<head>
+    <title>WEB PERHITUNGAN BMI</title>
+</head>
+
+<body>
+    <h2>Welcome</h2>
+    <h4>Welcome, <?php echo $_SESSION['username']; ?>! You're successfull to login !</h4>
+    <br />
+    <br />
+    <table>
+        <td><a href="logout.php">LOGOUT</a></td>
+        <!--<td><a href="menuperhitungan"-->
+    </table>
+</body>
+
 </html>
