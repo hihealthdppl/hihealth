64cab274f5275f6355f60d8d09f00e875dde836a
Hafiyyan Abdul Aziz <hafiyyan.123@gmail.com>
2020-04-07 19:57:32 +0700

    Perbaikan nonvegetarian

--- dashboard/perhitungan_kalori.php
+++ dashboard/perhitungan_kalori.php
@@ -2,10 +2,10 @@
 
 // Action Form
 
-if(isset($_POST['hitung'])){
-    
+if (isset($_POST['hitung'])) {
+
     //Input Form
-    
+
     $bb       = $_POST['bb'];
     $tb       = $_POST['tb'];
     $kelamin  = $_POST['kelamin'];
@@ -27,71 +27,71 @@ if(isset($_POST['hitung'])){
 
     */
 
-    if($kelamin=="Perempuan"){
-        
-        $BMR = 447.593+(9.247*$bb)+(3.098*$tb)-(4.33*$umur);
-        if($kegiatan=="TidakAktif"){
-            $TEE=$BMR*1.2;
-            $TEEc=$TEE*1000;
-        }
-        if($kegiatan=="CukupAktif"){
-            $TEE=$BMR*1.375;
-            $TEEc=$TEE*1000;
-        }
-        if($kegiatan=="Aktif"){
-            $TEE=$BMR*1.55;
-            $TEEc=$TEE*1000;
-        }
-        if($kegiatan=="SangatAktif"){
-            $TEE=$BMR*1.725;
-            $TEEc=$TEE*1000;
-        }
-        echo"Dengan Berat Badan Sebesar $bb Tinggi Badan Sebesar $tb <br>";
-        echo"BMR Anda sebesar $BMR <br>";
-        echo"Kebutuhan Kalori Harian Anda Adalah : $TEE kcals atau $TEEc cals <br>";
-        echo"<br>";
-        echo"Makanan Yang Disarankan Setiap Harinya Untuk Memenuhi Kebutuhan Kalori Anda Adalah : ";
-        echo"<br>";
-        echo"<br>";
-        echo"<br>";
-        if($pemakan=="NonVegetarian"){
-            ?>
-                <a href="nonvegetarian.php">LIHAT SOLUSI MAKANAN</a>
-                <?php
-            }
+    if ($kelamin == "Perempuan") {
 
-    }
-    else if($kelamin=="Laki"){
-        $BMR = 88.362+(13.397*$bb)+(4.799*$tb)-(5.677*$umur);
-        
-        if($kegiatan=="TidakAktif"){
-            $TEE=$BMR*1.2;
-            $TEEc=$TEE*1000;
+        $BMR = 447.593 + (9.247 * $bb) + (3.098 * $tb) - (4.33 * $umur);
+        if ($kegiatan == "TidakAktif") {
+            $TEE = $BMR * 1.2;
+            $TEEc = $TEE * 1000;
         }
-        if($kegiatan=="CukupAktif"){
-            $TEE=$BMR*1.375;
-            $TEEc=$TEE*1000;
+        if ($kegiatan == "CukupAktif") {
+            $TEE = $BMR * 1.375;
+            $TEEc = $TEE * 1000;
         }
-        if($kegiatan=="Aktif"){
-            $TEE=$BMR*1.55;
-            $TEEc=$TEE*1000;
+        if ($kegiatan == "Aktif") {
+            $TEE = $BMR * 1.55;
+            $TEEc = $TEE * 1000;
         }
-        if($kegiatan=="SangatAktif"){
-            $TEE=$BMR*1.725;
-            $TEEc=$TEE*1000;
+        if ($kegiatan == "SangatAktif") {
+            $TEE = $BMR * 1.725;
+            $TEEc = $TEE * 1000;
         }
-        
-        echo"Dengan Berat Badan Sebesar $bb Tinggi Badan Sebesar $tb <br>";
-        echo"BMR Anda sebesar $BMR <br>";
-        echo"Kebutuhan Kalori Harian Anda Adalah : $TEE kcals atau $TEEc cals <br>";
-        echo"<br>";
-        echo"Makanan Yang Disarankan Setiap Harinya Untuk Memenuhi Kebutuhan Kalori Anda Adalah : ";
-        echo"<br>";
-        echo"<br>";
-        echo"<br>";
+        echo "Dengan Berat Badan Sebesar $bb Tinggi Badan Sebesar $tb <br>";
+        echo "BMR Anda sebesar $BMR <br>";
+        echo "Kebutuhan Kalori Harian Anda Adalah : $TEE kcals atau $TEEc cals <br>";
+        echo "<br>";
+        echo "Makanan Yang Disarankan Setiap Harinya Untuk Memenuhi Kebutuhan Kalori Anda Adalah : ";
+        echo "<br>";
+        echo "<br>";
+        echo "<br>";
+        if ($pemakan == "NonVegetarian") {
+?>
+            <a href="nonvegetarian.php">LIHAT SOLUSI MAKANAN</a>
+<?php
+        }
+    } else if ($kelamin == "Laki") {
+        $BMR = 88.362 + (13.397 * $bb) + (4.799 * $tb) - (5.677 * $umur);
 
-        
+        if ($kegiatan == "TidakAktif") {
+            $TEE = $BMR * 1.2;
+            $TEEc = $TEE * 1000;
         }
+        if ($kegiatan == "CukupAktif") {
+            $TEE = $BMR * 1.375;
+            $TEEc = $TEE * 1000;
+        }
+        if ($kegiatan == "Aktif") {
+            $TEE = $BMR * 1.55;
+            $TEEc = $TEE * 1000;
+        }
+        if ($kegiatan == "SangatAktif") {
+            $TEE = $BMR * 1.725;
+            $TEEc = $TEE * 1000;
+        }
+
+        echo "Dengan Berat Badan Sebesar $bb Tinggi Badan Sebesar $tb <br>";
+        echo "BMR Anda sebesar $BMR <br>";
+        echo "Kebutuhan Kalori Harian Anda Adalah : $TEE kcals atau $TEEc cals <br>";
+        echo "<br>";
+        echo "Makanan Yang Disarankan Setiap Harinya Untuk Memenuhi Kebutuhan Kalori Anda Adalah : ";
+        echo "<br>";
+        echo "<br>";
+        echo "<br>";
     }
+}
+
+if ($TEE >= 1600 && $TEE < 1800) {
+    include "nonvegetarian.php";
+}
 
 ?>
